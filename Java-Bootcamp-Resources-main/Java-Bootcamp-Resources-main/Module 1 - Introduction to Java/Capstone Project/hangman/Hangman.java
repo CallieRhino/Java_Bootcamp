a12d4aa0e78e2bb9cae1ae00ff1fd9a28b06341a
Callie <c.p.vanhuyssteen@gmail.com>
2022-09-26 18:19:36 +0200

    Hangman first push

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Hangman {
 
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
@@ -68,16 +71,27 @@ public class Hangman {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
 
 
+        for (int i = 0; i < gallows.length; i++) {
+            System.out.println(gallows[i]);
+            System.out.println("Word: ");
+            System.out.println("Misses: ");
+            System.out.print("Guess: ");
+            String userGuess = scanner.nextLine();
+            System.out.println(userGuess);
+        }
 
+        scanner.close();
+    }
 
+    public static String randomWord(String[] words) {
+        Random rand = new Random();
+        int n = rand.nextInt(words.length);
+        return words[n];
     }
 
 }
-
-
-
-
 
