c0ad4fc8e6d30030c773009e8d248da2d70fb51a
Callie <c.p.vanhuyssteen@gmail.com>
2022-09-27 14:25:41 +0200

    Capstone project 1 complete

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
@@ -72,16 +72,35 @@ public class Hangman {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String word = randomWord(words);
+        System.out.println(word);
 
-
+        int turns = 0;
+        String misses = "";
+        String placeholders = printPlaceholders(word);
 
         for (int i = 0; i < gallows.length; i++) {
-            System.out.println(gallows[i]);
-            System.out.println("Word: ");
-            System.out.println("Misses: ");
+            if (isWin(placeholders)) {
+                System.out.println("You win!");
+                break;
+            } else if (turns == 6) {
+                System.out.println("RIP!");
+                break;
+            }
+            System.out.println(gallows[turns]);
+            System.out.println("Word: " + placeholders + "\n");
+            System.out.println("Misses: " + misses + "\n\n");
             System.out.print("Guess: ");
             String userGuess = scanner.nextLine();
-            System.out.println(userGuess);
+            checkGuess(userGuess, word);
+            
+            if (checkGuess(userGuess, word)) {
+                placeholders = updatePlaceholders(word, userGuess, placeholders);
+            } else {
+                misses = printMisses(userGuess, misses);
+                turns++;
+             }
+
         }
 
         scanner.close();
@@ -93,5 +112,48 @@ public class Hangman {
         return words[n];
     }
 
+    public static String printPlaceholders(String word) {
+        StringBuilder sb = new StringBuilder(word);
+        for (int i = 0; i < word.length(); i++) {
+            sb.setCharAt(i, '_');
+        }
+        return sb.toString();
+    }
+
+    public static boolean checkGuess(String userGuess, String word) {
+        char userChar = userGuess.charAt(0);
+        for (int i = 0; i < word.length(); i++) {
+            if (userChar == word.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String updatePlaceholders(String word, String userGuess, String placeholders) {
+        StringBuilder sb = new StringBuilder(placeholders);
+        char userChar = userGuess.charAt(0);
+        for (int i = 0; i < word.length(); i++) {
+            if (userChar == word.charAt(i)) {
+                sb.setCharAt(i, userChar);;
+            } 
+        }
+        return sb.toString();
+    }
+
+    public static String printMisses(String userGuess, String misses) {
+        StringBuilder sb = new StringBuilder(misses);
+        sb.append(userGuess.charAt(0));
+        return sb.toString();
+    }
+
+    public static boolean isWin(String placeholders) {
+        for (int i = 0; i < placeholders.toCharArray().length; i++) {
+            if (placeholders.charAt(i) == '_') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
