3f968bb27dc5b9445e34bfc9de05016f6406ed50
Callie <c.p.vanhuyssteen@gmail.com>
2022-09-28 11:32:26 +0200

    157 Setters

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Tasks/Section 7/Main.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Tasks/Section 7/Main.java
@@ -1,26 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car();
 
-        nissan.make = "Nissan";
-        nissan.price = 5000;
-        nissan.year = 2020;
-        nissan.color = "red";
+        Car nissan = new Car("Nissan", 5000, 2020, "red");
 
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 11000;
-        dodge.year = 2019;
-        dodge.color = "blue";
+        Car dodge = new Car("Dodge", 8500, 2019, "blue");
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price
-        + ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
+        nissan.setColor("Jet Black");
+        dodge.setColor("Jet Black");
 
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price
-        + ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
-   
+        nissan.setPrice(nissan.getPrice()/2);
 
+        dodge.setPrice(dodge.getPrice()/2);
+      
 
-        
+        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice()
+        + ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+
+        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice()
+        + ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
     }
 }
