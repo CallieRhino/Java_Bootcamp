42d9807fafb0017704669d4b9718abf5e1c4c28d
Callie <c.p.vanhuyssteen@gmail.com>
2022-09-28 11:53:25 +0200

    workbook 7.3

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
@@ -1,10 +1,16 @@
+
 public class Main {
   
     public static void main(String[] args) {
 
+        Person rayanSlim = new Person("Rayan Slim", "Canadian", 
+        "01/01/1111", 5);
 
-      
-      
+        rayanSlim.setSeatNumber(10);;
+
+        System.out.println("Name: " + rayanSlim.getName() + "\n" + "Nationality: " +
+         rayanSlim.getNationality() + "\n" + "Date of Birth: " + rayanSlim.getDateOfBirth() +
+          "\n" + "Seat Number: " + rayanSlim.getSeatnumber() + "\n");
       
     }
   
---
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person.java
@@ -0,0 +1,47 @@
+public class Person {
+    private String name;
+    private String nationality;
+    private String dateOfBirth;
+    private String[] passport;
+    private int seatNumber;
+
+    Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    public String getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public int getSeatnumber() {
+        return this.seatNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setDaterOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+}
